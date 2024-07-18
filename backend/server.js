83b30588dbcdaf3b727dd111d0e785ce70f2ef04
George-Okumu <gokumu12@gmail.com>
2024-07-18 13:03:00 +0300

    feat: disabled writing into db.json

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 // JSON Server module
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const fs = require('fs');
+const path = require('path');
 const middlewares = jsonServer.defaults();
+const data = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json'), 'utf-8'));
+const router = jsonServer.router(data);
+
 
 server.use(middlewares);
-// Add this before server.use(router)
+
 server.use(
 	// Add custom route here if needed
 	jsonServer.rewriter({
